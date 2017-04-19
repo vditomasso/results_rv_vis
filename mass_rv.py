0e92d8be4cf2f3fcaa09dfa05ea2338746fe94de
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-04-19 12:10:09 -0400

    Gets rid of axes, no blue star

--- mass_rv.py
+++ mass_rv.py
@@ -1,25 +1,6 @@
-import NIRSPEC
 import pandas as pd
 import numpy as np
 import astrometry
-
-def mass_rv(obj_filename,obj_name):
-
-	df=pd.read_csv('/Users/victoriaditomasso/Desktop/RV_comp_spectra/All_RV_comps_DF.txt',sep='\t')
-	
-	RV = []
-	error = []
-	
-	for i, row in df.iterrows() :
-	
-		rv_and_error = NIRSPEC.main(['',row['filename'],row['std_rv'],row['std_unc'],obj_filename,1])
-		RV.append(rv_and_error[0])
-		error.append(rv_and_error[1])
-		
-	df['obj_rv'] = RV
-	df['obj_unc'] = error
-	
-	df.to_csv(str(obj_name)+'_against_all_comps.txt',sep='\t')
 	
 def avg_rv(path_to_df):
 
@@ -37,4 +18,6 @@ def avg_rv(path_to_df):
 
 	calc_rv_unc = astrometry.wstddev(RV,eRV)
 	
-	print "Avg RV = " + str(calc_rv_unc[0]) + " +/- " + str(calc_rv_unc[1])
+	# print "Avg RV = " + str(calc_rv_unc[0]) + " +/- " + str(calc_rv_unc[1])
+
+	return calc_rv_unc
--- rv_results_vis.py
+++ rv_results_vis.py
@@ -16,36 +16,37 @@ def rv_vis(path_to_rv_results_minus_df):
 
         spec_type = float(spec_type)
 
-    # plt.scatter(spec_type,row['obj_rv'],color='black')
-
-    # 		plt.errorbar(spec_type, row['obj_rv'], yerr=row['obj_unc'],color='black',alpha=0.7)
+        plt.scatter(spec_type,row['obj_rv'],color='black')
+        plt.errorbar(spec_type, row['obj_rv'], yerr=row['obj_unc'],color='black',alpha=0.7)
 
     beginning_of_path = path_to_rv_results_minus_df.split('/')[6]
     obj_name = beginning_of_path.split('_')[0]
     # 	print obj_name
 
     calc_rv_unc = mass_rv.avg_rv(path_to_rv_results_minus_df)
-    	rv = calc_rv_unc[0]
-    	unc = calc_rv_unc[1]
+    rv = calc_rv_unc[0]
+    unc = calc_rv_unc[1]
 
     plt.annotate(str(obj_name), xycoords='axes fraction', xy=(.77, .05), size=15)
-    eleven = float(13)
-    obj_spec_type_position = float(obj_spec_type) - 5.0
-    xmax = obj_spec_type_position / eleven
+    # eleven = float(13)
+    # obj_spec_type_position = float(obj_spec_type) - 5.0
+    # xmax = obj_spec_type_position / eleven
 
     plt.xlim([5, 18])
     plt.ylim([-1.5, 6.5])
     # 	plt.axhline(y=rv, xmax=xmax, color='blue', ls='--',alpha=0.5)
-    plt.fill_between(range(5, 19), rv + unc, rv - unc, interpolate=True, alpha=0.3)
+    plt.fill_between(range(5, 19), rv + unc, rv - unc, interpolate=True, alpha=0.3, color='grey')
 
     spec_type_range = [6, 8, 10, 12, 14, 16, 18]
     labels = ['M6', 'M8', 'L0', 'L2', 'L4', 'L6', 'L8']
     plt.xticks(spec_type_range, labels)
 
-    plt.xlabel('Spectral Type')
-    plt.ylabel('Calculated RV')
+    plt.xlabel("Spectral Type of RV Standard")
+    plt.ylabel('Calculated RV (km/s)')
 
-    # 	plt.savefig(str(obj_name)+'_calculated_rvs.png')
-    plt.savefig(str(obj_name) + '_rv_results.png')
+    for spine in plt.gca().spines.values():
+        spine.set_visible(False)
+
+    # plt.savefig(str(obj_name) + '_rv_results.png')
 
     plt.show()
