defc3d19a2d66def4a25c563c5850c46aa27b2dc
vditomasso <victoria.ditomasso@macaulay.cuny.edu>
2017-04-19 13:42:21 -0400

    Gets rid of top and right axes, labels axes as ax

--- rv_results_vis.py
+++ rv_results_vis.py
@@ -8,6 +8,8 @@ def rv_vis(path_to_rv_results_minus_df):
 
     plt.clf()
 
+    ax = plt.subplot(111)
+
     for i, row in df.iterrows():
         spec_type = row['std_opt_spec_type']
         spec_type = spec_type.replace('M', '0').replace('L', '1').replace('T', '2')
@@ -16,8 +18,8 @@ def rv_vis(path_to_rv_results_minus_df):
 
         spec_type = float(spec_type)
 
-        plt.scatter(spec_type,row['obj_rv'],color='black')
-        plt.errorbar(spec_type, row['obj_rv'], yerr=row['obj_unc'],color='black',alpha=0.7)
+        ax.scatter(spec_type,row['obj_rv'],color='black')
+        ax.errorbar(spec_type, row['obj_rv'], yerr=row['obj_unc'],color='black',alpha=0.7)
 
     beginning_of_path = path_to_rv_results_minus_df.split('/')[6]
     obj_name = beginning_of_path.split('_')[0]
@@ -32,20 +34,21 @@ def rv_vis(path_to_rv_results_minus_df):
     # obj_spec_type_position = float(obj_spec_type) - 5.0
     # xmax = obj_spec_type_position / eleven
 
-    plt.xlim([5, 18])
-    plt.ylim([-1.5, 6.5])
+    ax.set_xlim([5, 18])
+    ax.set_ylim([-1.5, 6.5])
     # 	plt.axhline(y=rv, xmax=xmax, color='blue', ls='--',alpha=0.5)
-    plt.fill_between(range(5, 19), rv + unc, rv - unc, interpolate=True, alpha=0.3, color='grey')
+    ax.fill_between(range(5, 19), rv + unc, rv - unc, interpolate=True, alpha=0.3, color='grey')
 
     spec_type_range = [6, 8, 10, 12, 14, 16, 18]
     labels = ['M6', 'M8', 'L0', 'L2', 'L4', 'L6', 'L8']
-    plt.xticks(spec_type_range, labels)
+    ax.set_xticks(spec_type_range)
+    ax.set_xticklabels(labels)
 
-    plt.xlabel("Spectral Type of RV Standard")
-    plt.ylabel('Calculated RV (km/s)')
+    ax.set_xlabel("Spectral Type of RV Standard")
+    ax.set_ylabel('Calculated RV (km/s)')
 
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
 
     # plt.savefig(str(obj_name) + '_rv_results.png')
 
